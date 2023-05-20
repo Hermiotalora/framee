922e19dd7091b91c2cef9e00c7160958acdd3bb8
hermiotalora <hermiotalora@gmail.com>
2023-05-20 13:39:34 -0300

    proyects

---
+++ app.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from gui_app import Frame, barra_menu
+
+def main():
+    
+    root = tk.Tk() 
+    root.title('FilmView')
+    root.resizable(0,0)
+
+    barra_menu(root)
+
+    app = Frame(root = root)
+
+    app.mainloop()
+
+if __name__ == '__main__':
+     main()
